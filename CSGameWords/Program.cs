9cc42232a965f4ab948881648b118b6afff8ade8
Solovova <vsolo0@gmail.com>
2023-05-31 10:00:12 +0300

    0.01

--- CSGameWords/Program.cs
+++ CSGameWords/Program.cs
@@ -1,3 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+void ToConsole(string d) {
+    int ind = 0, line = 0, n =0;
+    Console.Write(" ");
+    while (ind<d.Length) {
+        int maxN = 4;
+        if (line % 2 == 0) {
+            maxN = 3;
+        }
+        
+        Console.Write(d[ind]);
+        Console.Write(" ");
+        ind++;
+        n++;
+        if (n == maxN) {
+            Console.WriteLine();
+            line++;
+            if (line % 2 == 0) {
+                Console.Write(" ");
+            }
+
+            n = 0;
+        }
+    }
+}
+
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+string data = "КОСДРАЛУАЄЕПІАІЕИЕДХОИМВЩІЛАВОИАГОІОТПЖМЇЕЯАИРСГЄВРЩИЙУНПДАЕНОІ";
+ToConsole(data);
